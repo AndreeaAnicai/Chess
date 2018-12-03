3a027a1a5f6af40948aad520315822d9c1cd0f8d
Andreea Anicai <andreea_elena.anicai@yahoo.com>
2018-12-03 02:03:36 +0000

    Added constants in utlities

---
+++ Utilities.h
@@ -0,0 +1,15 @@
+/*****************************************************/ 
+/*  This is the header file Utilities.h
+	This is where constant variables are declared
+*/
+/*****************************************************/
+
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+const bool WHITE = true;
+const bool BLACK = false;
+const int X_MAX = 8;
+const int Y_MAX = 8;
+
+#endif //UTLITIES_H
