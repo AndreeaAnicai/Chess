f7885c1d8def519fb101d4cfe36c564de63b476f
Andreea Anicai <andreea_elena.anicai@yahoo.com>
2018-12-03 02:00:54 +0000

    Piece header with functions

--- Piece.h
+++ Piece.h
@@ -1,3 +1,58 @@
-/* This is the header file for the Piece class*/
+/*****************************************************/ 
+/*  This is the header file Piece.h
+	This is the interface for the class Piece
+	This is intended to be used as a base class to
+	derive classes for all chess pieces 
+*/
+/*****************************************************/
 
-#ifndef 
+#ifndef PIECE_H
+#define PIECE_H
+
+#include "Utilities.h"
+using namespace std;
+
+class Piece 
+{
+public:
+	// Default contstructor
+    Piece(int xCoord, int yCoord, bool colour, char name);
+    // Default deconstructor
+    virtual ~Piece();
+
+    // Check colour of piece, returns 1 if white and 0 is black
+    bool isWhite();
+    // Returns letter that represents piece name
+    char getPieceName();
+    // Returns colour as a char W/B
+    char getPieceColour();
+    // Counter that increments whenever piece moves
+    void moveCounter();
+    // Return total number of moves for piece
+    int getMoveCounter();
+    // Resets x and y coordinates for piece
+    void setXYCoord(int newX, int newY);
+    /* 
+    	Function that checks if move is valid taking into account
+    	- board spatial limits 
+		- individual piece allowed movement - isMoveLegal()
+		- whether there are blocking pieces - isPathClear()
+    */ 
+    bool isValidMove(int destination[2], Piece* board[X_MAX][Y_MAX]);
+    // Check if trajectory is blocked by any piece
+    virtual bool isPathClear(int xTranslation, int yTranslation, ChessPiece* targetPiece, ChessPiece *board[X_MAX][Y_MAX]);
+    // Check if the move is allowed by each piece's rules
+    virtual bool isMoveLegal(int xTranslation, int yTranslation) const = 0;
+
+protected:
+    int xCoord; //file
+    int yCoord; //rank;
+    bool isWhite;
+    bool isFirstMove;
+    int moveCount;
+    char name; // W or B
+
+
+}; // Piece class
+
+#endif //PIECE_H
